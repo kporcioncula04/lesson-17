a7e4fca4b925b3e74e3c4925d5a0cb76d26a57ea
Karolina <kporcioncula.04@gmail.com>
2021-05-24 16:18:32 -0400

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyCukHC3nUY3Vn-H1hkk8YSw7RW3G9Q72hc",
+  authDomain: "crwn-clothing-db-a8379.firebaseapp.com",
+  databaseURL: "https://crwn-clothing-db-a8379-default-rtdb.firebaseio.com",
+  projectId: "crwn-clothing-db-a8379",
+  storageBucket: "crwn-clothing-db-a8379.appspot.com",
+  messagingSenderId: "94491010909",
+  appId: "1:94491010909:web:3a6ebc647bd12b9445eab4",
+  measurementId: "G-VN6VN8P45C"
 };
 
 firebase.initializeApp(config);
